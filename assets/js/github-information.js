29b6785241164e2c399714d0bb69d590c009585c
markohautala <markohautala@live.se>
2024-03-26 11:12:34 +0100

    Add GitHub user information retrieval and error handling

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -9,4 +9,20 @@ function fetchGitHubInformation(event) {
         `<div id="loader">
             <img src="assets/images/loader.gif" alt="loading..." />
         </div>`);
-}
+
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`)
+    ).then(
+        function (response) {
+            var userData = response;
+            $("#gh-user-data").html(userInformationHTML(userData));
+        }, function (errorResponse) {
+            if (errorResponse.status === 404) {
+                $("gh-user-data").html(`<p>No info found for user ${username}</p>`);
+            } else {
+                console.log(errorResponse);
+                $("#gh-user-data").html(`<p>Error: ${errorResponse.responseJSON.message}</p>`);
+            }
+
+            });
+        }
