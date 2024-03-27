460fccfb2e027cd7ce2efc2ca7f58bedb41c0a3b
markohautala <markohautala@live.se>
2024-03-27 21:06:27 +0100

    Update sendEmail.js and contact.html

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,20 @@
-console.log("sendEmail.js loaded");
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'service_0fh2hvc';
+   const templateID = 'template_spa2kvb';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
